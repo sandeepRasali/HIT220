2c5a7d1c96eff4284725e8bbeb193380199ca2b7
Alex <alex.phan2905@gmail.com>
2022-10-27 23:23:58 +0930

    pseudocode for next site; merge sort updated

--- a3-part-a.py
+++ a3-part-a.py
@@ -3,7 +3,8 @@ import math
 
 # Node Representation
 class Node:
-    def __init__(self, data, weights):
+    def __init__(self, source, data, weights):
+        self.source = source
         self.data = data
         self.weights = weights
         self.next = None
@@ -16,15 +17,61 @@ class Graph:
 
     def add_edge(self, v1, v2, v1_weights = None, v2_weights = None):
         # connect second vertex to first vertex at first vertex's position in vertex list
-        node = Node(v2, v2_weights)
+        node = Node(v1, v2, v2_weights)
         node.next = self.list[v1 - 1]
         self.list[v1 - 1] = node
 
         # connect second vertex to first vertex at second vertex's position in vertex list
-        node = Node(v1, v1_weights)
+        node = Node(v2, v1, v1_weights)
         node.next = self.list[v2 - 1]
         self.list[v2 - 1] = node
 
+    # **Psuedocoded** Find Next Site Algorithm:
+
+    # Breadth First Search to find the adjacent nodes in the graph
+    # def bfs(self, startNode):
+    #     visited = [False] * self.vertices # mark all the vertices as not visited
+
+    #     queue = [] # queue for BFS
+
+    #     append startNode to queue
+    #     mark startNode as visited and enqueue it
+        
+    #     while queue:
+    #         dequeue a vertex from queue and store in an array
+    #         then get all adjacent veritices of the dequeued vertex
+
+    #         if adjacent has not been visited:
+    #             then mark it as visited and enqueue it
+            
+    #     return the adjacent vertices array
+
+    # def next_site(previous_site, previous_number):
+    #     # call bfs() to get the adjacent sites of previous_site
+    #     bfs(previous_site)
+        
+    #     for i in array:
+    #         key = i
+    #         row = node_list.get(key)
+            
+    #         for x in row:
+    #             node_weight = row[1]  # getting the croc sighting value for each adjacent nodes
+        
+    #     then compare the nodes to get the node with the highest node_weight (croc sighting)
+        
+    #     if node_weight is similar for adjacent nodes
+    #         for x in row:
+    #             node_distance = row[2]
+    #         get node with minimum distance
+            
+    #     # call bfs() recursively to start searching from new node.   
+    #     bfs(newNode)  # newNode = node with highest weight
+        
+    #     nearest_site = node
+
+    #     return node and location
+
+# Data Handling
 class CrocData:
     def __init__(self):
         self.node_list = []
@@ -55,9 +102,8 @@ class CrocData:
     
     def display_graph(self):
         for i in range(self.graph.vertices):
-            print("Vertex " + str(i + 1) + ":", end = "")
-
-            vertex = self.graph.list[i] # get vertex
+            vertex = self.graph.list[i]
+            print("Vertex " + str(vertex.source) + ":", end = "")
 
             while vertex:
                 print(" -> {}".format(vertex.data) + ", {}".format(vertex.weights), end = "")
@@ -77,7 +123,7 @@ def add_sighting(graph, site: int, source: int = None, distance: float = 1, wate
                 vertex = vertex.next
     else:
         if source <= graph.vertices:
-            # prepare weights with
+            # prepare weights of new edge
             previous_sightings = 0
 
             for i in range(graph.vertices):
@@ -87,7 +133,7 @@ def add_sighting(graph, site: int, source: int = None, distance: float = 1, wate
                     if vertex.data == source:
                         previous_sightings = vertex.weights["sightings"]
 
-                        break
+                        break # break loop early if matching vertex is found
                 
                     vertex = vertex.next
 
@@ -102,19 +148,53 @@ def add_sighting(graph, site: int, source: int = None, distance: float = 1, wate
             graph.add_edge(site, source, location_weights, previous_weights)
         else:
             print("\033[91m{}\033[00m".format("add_sighting() error: source site does not exist\n"))
-            
+
+# Merge Sort Algorithm adapted from https://www.geeksforgeeks.org/merge-sort/
+def merge_sort(array): # input should be an adjacency list with a "sightings" weight on edges
+    if len(array) > 1: # base case
+        mid = len(array) // 2 # midpoint
+        left = array[:mid] # left half
+        right = array[mid:] # right half
+
+        merge_sort(left) # sort left half
+        merge_sort(right) # sort right half
+
+        i = j = k = 0 # iterators
+        
+        while i < len(left) and j < len(right):
+            if left[i].weights["sightings"] > right[j].weights["sightings"]:
+                array[k] = left[i]
+                i += 1
+            else:
+                array[k] = right[j]
+                j += 1
+            k += 1
+
+        while i < len(left):
+            array[k] = left[i]
+            i += 1
+            k += 1
+        
+        while j < len(right):
+            array[k] = right[j]
+            j += 1
+            k += 1
 
 # Driver Code
 if __name__ == '__main__':
     # Import Datasets
     cm = CrocData()
     cm.import_data('CrocDataNodes.csv')
+
+    # Show Data (comment/uncomment as needed)
     # print("Croc Nodes Data:\n")
     # cm.read_data()
     # print("\n")
 
     edges = CrocData()
     edges.import_data('CrocDataEdges.csv')
+
+    # Show Data (comment/uncomment as needed)
     # print("Croc Edges Data:\n")
     # edges.read_data()
     # print("\n")
@@ -144,10 +224,17 @@ if __name__ == '__main__':
         v2_weights = {"distance": distance, "water": water, "sightings": v2_sightings}
         cm.graph.add_edge(v1, v2, v1_weights, v2_weights)
     
-    # print("Croc Adjacency List:")
+    # Show Graph (comment/uncomment as needed)
+    # print("Croc Adjacency List:\n")
     # cm.display_graph()
 
-    add_sighting(cm.graph, 33, 32)
+    add_sighting(cm.graph, 12) # update an existing site with a new sighting (compare old graph with updated graph to see the change)
+    add_sighting(cm.graph, 33, 12, 2.4, False) # add a new site with a new sighting and connect it to Vertex 12
 
-    print("Croc Adjacency List with Added Sightings:\n")
+    # Show Graph (comment/uncomment as needed)
+    # print("Croc Adjacency List with Added Sightings:\n")
+    # cm.display_graph()
+
+    merge_sort(cm.graph.list)
+    print("Merge Sorted Adjacency List:\n")
     cm.display_graph()
